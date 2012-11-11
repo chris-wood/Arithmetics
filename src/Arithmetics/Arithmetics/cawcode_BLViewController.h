5a60b01c58c0b2c34752ed5e51900e92dc16e35b
Christopher Wood <christopherwood07@gmail.com>
2012-11-11 10:17:26 -0500

    Updated code a little - did some reading too...

--- src/Arithmetics/Arithmetics/cawcode_BLViewController.h
+++ src/Arithmetics/Arithmetics/cawcode_BLViewController.h
@@ -8,7 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface cawcode_BLViewController : UIViewController
+@interface cawcode_BLViewController : UIViewController {
+	// Instance variables represent an implementation detail,
+	// and should typically not be accessed outside of the class
+	// itself. Moreover, you can declare them in the implementation
+	// block or synthesize them using declared properties. Typically
+	// you should not, therefore, declare instance variables in the
+	// public interface and so you should omit the braces.
+}
 - (IBAction)changeGreeting:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
